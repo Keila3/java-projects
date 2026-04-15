8aa990cb1fec6073ff8f23ffe8b19bd115f59c00
keila <kbkeilabarbosa@gmail.com>
2026-04-15 19:03:50 +0000

    loop + corte e exibição

--- descodificador-cvs/src/main.java
+++ descodificador-cvs/src/main.java
@@ -11,13 +11,22 @@ public class Main {
         // Abertura segura
         try{
             BufferedReader leitor = new BufferedReader(new FileReader(caminho));
+          
+          // Processamento - Loop
+             String linha;
+             while ((linha = leitor.readLine()) != null) { 
+         
+          // Corte e exibição
+            String[] dados = linha.split(",");
+            System.out.println("Nome: " + dados[0] + "| Idade: " + dados[1]);
+        }
+
         }
         catch (IOException e){
             System.out.println("Erro!");
         }
 
-        // Processamentp - Loop
-
-        // Corte e exibição
+      
+        
     }
 }
