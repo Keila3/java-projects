b99f6e0408f86814624e2b527805adc6c49197aa
keila <kbkeilabarbosa@gmail.com>
2026-04-15 22:49:11 +0000

    correção de erro (run)

--- descodificador-cvs/src/Main.java
+++ descodificador-cvs/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args)
     {
         // Configuração
-        String caminho = "dados.csv";
+        String caminho = "descodificador-cvs/dados.csv";
 
         // Abertura segura
         try{
@@ -20,10 +20,11 @@ public class Main {
             String[] dados = linha.split(",");
             System.out.println("Nome: " + dados[0] + "| Idade: " + dados[1]);
         }
+             leitor.close();
 
         }
         catch (IOException e){
-            System.out.println("Erro!");
+            System.out.println("Erro ao ler o arquivo!");
         }
 
       
