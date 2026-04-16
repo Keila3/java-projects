17d29c1636611ddec35bef29f52ed89a801a4732
keila <kbkeilabarbosa@gmail.com>
2026-04-16 02:44:48 +0000

    implementa a classe FilaPrioridade com métodos para adicionar, remover e exibir tarefas

--- Fila-Prioridade/src/FilaPrioridade.java
+++ Fila-Prioridade/src/FilaPrioridade.java
@@ -1,3 +1,32 @@
+import java.util.*;
 public class FilaPrioridade {
-    
+    private ArrayList<Tarefa> tarefas;
+
+    // Construtor
+    public FilaPrioridade(){
+        this.tarefas = new ArrayList<>();
+    }
+
+    // Método para adicionar uma tarefa à fila de prioridade
+    public void adicionarTarefa(Tarefa tarefa){
+        tarefas.add(tarefa);
+        Collections.sort(tarefas);// Ordena as tarefas com base na prioridade
+    }
+
+    // Método para remover e retornar a tarefa com maior prioridade
+    public Tarefa proximo(){
+        if (tarefas.isEmpty()) {
+            return null; // Retorna null se a fila estiver vazia
+        }
+        return tarefas.remove(0); // Remove e retorna a tarefa com maior prioridade (primeira
+    }
+
+    // Método para exibir as tarefas na fila de prioridade
+    public void exibirTarefas(){
+        for (Tarefa t : tarefas){
+            System.out.println(t);
+        }
+    }
+
 }
+
