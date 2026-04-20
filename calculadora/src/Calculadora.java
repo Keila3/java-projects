89a3ba0762bd55b6207d1844a9e192b8d539dcdf
keila <kbkeilabarbosa@gmail.com>
2026-04-20 21:31:17 +0000

    implementa a classe Calculadora com métodos de operação e ajusta o menu interativo para realizar cálculos-

---
+++ calculadora/src/Calculadora.java
@@ -0,0 +1,24 @@
+public class Calculadora{
+   double numero1;
+   double numero2;
+
+   public double somar(){
+    return numero1 + numero2;
+   }
+
+   public double subtrair(){
+    return numero1 - numero2;
+   }
+
+   public double multiplicar(){
+    return numero1 * numero2;
+   }
+
+   public double dividir(){
+      if (numero2 == 0) {
+        System.out.println("Erro: Divisão por zero!");
+        return 0; 
+    }
+    return numero1 / numero2;
+   }
+}
--- calculadora/src/main.java
+++ calculadora/src/main.java
@@ -1,24 +1,49 @@
 import java.util.Scanner;
 
+
 public static void main(String[] args) {
+    Scanner leitor = new Scanner(System.in);
+    Calculadora calc = new Calculadora();
+
     do{
         System.out.println("Digite o número da opção deseja: ");
         System.out.println("|1| Somar: ");
         System.out.println("|2| Subtarir: ");
         System.out.println("|3| Multiplicar: ");
         System.out.println("|4| Dividir: ");
-        Scanner opcaoUsuario = new Scanner(System.in);
-        int opcao = opcaoUsuario.nextInt();
+        int opcao = leitor.nextInt();
         if (opcao <= 4 && opcao >= 1){
             System.out.println("Digite o primeiro número: ");
-            Scanner primeiroNumero = new Scanner(System.in);
-            int num1 = primeiroNumero.nextInt();
-            Scanner SegundoNumero = new Scanner(System.in);
-            int num2 = SegundoNumero.nextInt();
+            double num1 = leitor.nextDouble();
+            double num2 = leitor.nextDouble();
+            calc.numero1 = num1;
+            calc.numero2 = num2;
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("O resultado é: " + calc.somar());
+                    break;
+                case 2:
+                    System.out.println("O resultado é: " + calc.subtrair());
+                    break;
+                case 3: 
+                System.out.println("O resultado é: " + calc.multiplicar());
+                     break;
+                case 4:
+                    System.out.println("O resultado é: " + calc.dividir());
+                    break;
+            
+                default:
+                    System.out.println("Erro! opção invalida");
+                    break;
+            }
+            System.out.println("Deseja continuar? [S/N] ");
             
         }
 
 
 
+    } while () {
+        
     }
 }
